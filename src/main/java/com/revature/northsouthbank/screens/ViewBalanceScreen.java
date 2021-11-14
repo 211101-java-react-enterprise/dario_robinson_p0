8084dd55f7b5d15efef0caac61170577fcd61f9d
dario robinson <dariorobinson@darios-air.lan>
2021-11-14 09:13:50 -0800

    Remove ds_store

--- src/main/java/com/revature/northsouthbank/screens/ViewBalanceScreen.java
+++ src/main/java/com/revature/northsouthbank/screens/ViewBalanceScreen.java
@@ -1,4 +1,10 @@
 package com.revature.northsouthbank.screens;
 
 public class ViewBalanceScreen {
+    int balance = 0;
+
+    public void viewBalance() {
+        System.out.println("Hello, welcome to NorthSouth Bank!");
+        System.out.println("Your balance is " + "$" + balance);
+    }
 }
