930e4ba774960949fbecc60700f69d65b1310a6b
dario robinson <dariorobinson@darios-air.lan>
2021-11-15 21:01:50 -0800

    added try/catch blocks to withdraw/deposit screens. will catch NumberFormatExceptions

--- src/main/java/com/revature/northsouthbank/screens/DepositScreen.java
+++ src/main/java/com/revature/northsouthbank/screens/DepositScreen.java
@@ -18,14 +18,24 @@ public class DepositScreen extends Screen {
 
     @Override
     public void render() throws Exception {
-        double depositAmount;
+        try {
+            double depositAmount;
         System.out.println("Hello! Your current balance is " + "$" + balance + ".");
+        System.out.println(" ");
         System.out.print("How much money would you like to deposit today?\n" +
                             "Enter an amount here > ");
         depositAmount = Double.parseDouble(consoleReader.readLine());
         System.out.println("You have deposited " + "$" + depositAmount + " into your account!");
+        System.out.println(" ");
         balance = balance + depositAmount;
+        System.out.println("Your new balance is: " + "$" + balance);
+        System.out.println(" ");
         System.out.println("Taking you back to dashboard...");
+        } catch (NumberFormatException n) {
+            System.out.println("Invalid command. Please try again.");
+            System.out.println(" ");
+            router.navigate("/deposits");
+        }
 
         // must add a way to get balance from somewhere and add deposit amount from it
         // to create a new balance (balance after adding deposit)
--- src/main/java/com/revature/northsouthbank/screens/WithdrawScreen.java
+++ src/main/java/com/revature/northsouthbank/screens/WithdrawScreen.java
@@ -19,20 +19,29 @@ public class WithdrawScreen extends Screen {
 
     @Override
     public void render() throws Exception {
-        double withdrawAmount;
-        System.out.println("Hello! Your current balance is " + "$" + balance + ".");
-        System.out.print("How much money would you like to withdraw today?\n" +
-                "Enter an amount here > ");
-        withdrawAmount = Double.parseDouble(consoleReader.readLine());
-        if (withdrawAmount >= 0.00 && balance > withdrawAmount) {
-            System.out.println("You have withdrawn " + "$" + withdrawAmount + " from your account!");
-            balance = balance - withdrawAmount;
-            System.out.println("Your new balance is: " + "$" + balance);
-            System.out.println("Taking you back to dashboard...");
-        } else if (balance < withdrawAmount) {
-            System.out.println("Cannot withdraw; insufficient funds. Please try again.");
+        try {
+            double withdrawAmount;
+            System.out.println("Hello! Your current balance is " + "$" + balance + ".");
+            System.out.println(" ");
+            System.out.print("How much money would you like to withdraw today?\n" +
+                    "Enter an amount here > ");
+            withdrawAmount = Double.parseDouble(consoleReader.readLine());
+            if (withdrawAmount >= 0.00 && balance > withdrawAmount) {
+                System.out.println("You have withdrawn " + "$" + withdrawAmount + " from your account!");
+                System.out.println(" ");
+                balance = balance - withdrawAmount;
+                System.out.println("Your new balance is: " + "$" + balance);
+                System.out.println(" ");
+                System.out.println("Taking you back to dashboard...");
+            } else if (balance < withdrawAmount) {
+                System.out.println("Cannot withdraw; insufficient funds. Please try again.");
+                System.out.println(" ");
+                router.navigate("/withdrawals");
+            }
+        } catch (NumberFormatException n) {
+            System.out.println("Invalid command. Please try again.");
+            System.out.println(" ");
             router.navigate("/withdrawals");
         }
-
     }
 }
